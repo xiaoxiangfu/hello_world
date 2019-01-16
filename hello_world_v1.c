c574ba53b9acbc12e884ad12816dda144d1d9617
Zhi Li <xiaoxiang.fu@163.com>
2019-01-16 17:39:55 +0800

    clean \"

--- hello_world_v1.c
+++
@@ -1,7 +0,0 @@
-main() {
-  extern a, b, c;
-  putchar(a); putchar(b); putchar(c); putchar(‘!*n’);
-}
-a ‘hell’;
-b ‘o, w’;
-c ‘orld’;
--- hello_world_v2.c
+++ hello_world_v2.c
@@ -1,3 +1,3 @@
 main() {
-  printf(“hello, world”);
+  printf("hello, world");
 }
